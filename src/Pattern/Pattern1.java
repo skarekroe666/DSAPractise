2988eb683ddecb2bc06824976c3f67232e390411
sanskar <sanskar006@gmail.com>
2025-03-12 18:16:25 +0530

    pattern questions

---
+++ src/Pattern/Pattern1.java
@@ -0,0 +1,34 @@
+package Pattern;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        pattern3(4);
+    }
+
+    static void pattern1(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j <= n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
